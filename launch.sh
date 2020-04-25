ec3f797981827001dc175fd127d9d55be587aea8
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-04-25 20:55:21 +0200

    upgrading my dockerized ide

--- launch.sh
+++ launch.sh
@@ -16,5 +16,8 @@ sed -i "s#OPERATOR_GID_JINJA2_VAR#$(id -g)#g" ./.env
 # Setting secrets home outside container
 sed -i "s#SECRETS_HOME_JINJA2_VAR#${SECRETS_HOME}#g"
 
+
+echo 'dummy value for the GitLab API Token' > $(pwd)/secret-manager/gitservice.api.token
+
 docker-compose -f docker-compose.ide.yml build
 docker-compose -f docker-compose.ide.yml up -d
