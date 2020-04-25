1c7e1899456b444f596c731a17ee4720887e285e
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-04-25 20:20:58 +0200

    upgrading my dockerized ide

---
+++ launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+export SECRETS_HOME=$(mktemp -d /tmp/pegasus.ide.XXXXXXXXXX)
+echo ${SECRETS_HOME} > ide.secrets.vault
+
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- #
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- #
+# Contianer User UID GID mapping to host configuration
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- #
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- #
+
+# Setting linux user uid and gid inside container
+sed -i "s#OPERATOR_UID_JINJA2_VAR#$(id -u)#g" ./.env
+sed -i "s#OPERATOR_GID_JINJA2_VAR#$(id -g)#g" ./.env
+# Setting secrets home outside container
+sed -i "s#SECRETS_HOME_JINJA2_VAR#${SECRETS_HOME}#g"
+
+docker-compose -f docker-compose.ide.yml build
+docker-compose -f docker-compose.ide.yml up -d
